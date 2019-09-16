f27da148ec9115a2def2336b96b87649fd4b1281
sdddoliver <55111319+sdddoliver@users.noreply.github.com>
2019-09-16 16:21:35 -0500

    Gave up on vst
    vsts sucks

---
+++ server.r
@@ -0,0 +1,34 @@
+library(shiny)
+library(ggplot2)
+
+# Define server logic required to draw a histogram
+function(input, output, session) {
+  # Combine the selected variables into a new data frame
+  selectedData <- reactive({
+    iris[, c(input$xcol, input$ycol)]
+  })
+  
+  clusters <- reactive({
+    kmeans(selectedData(), input$clusters)
+  })
+  
+  output$plot1 <- renderPlot({
+    #new colors, it's all purple
+    palette(c("#9803fc", "#ba03fc", "#d703fc", "#fc03c6", "#6b023e", "#4d0b43",
+              "#2c0b4d", "#9693e6", "#999999"))
+    par(mar = c(5.1, 4.1, 0, 1),
+        bg = "#fc03c6")
+    plot(selectedData(),
+         col = clusters()$cluster,
+         pch = 20,
+         cex = 3
+    )
+    points(clusters()$centers,
+           pch = 4,
+           cex = 4,
+           lwd = 4,
+           bg = "#2c0b4d", 
+           col = "#9693e6")
+  })
+  
+}
---
+++ ui.r
@@ -0,0 +1,58 @@
+library(shiny)
+library(ggplot2)
+library(DT)
+
+shinyUI(fluidPage(
+  theme = "prtty.css",
+  tags$head(tags$style(
+    HTML(
+      "
+    @import url('//fonts.googleapis.com/css?family=Modak&display=swap');
+      h1 {
+        font-family: 'Modak', cursive;
+        font-weight: 500;
+        line-height: 1.1;
+        color: #e8b715;
+      }
+    "
+    )
+  )),
+  
+  navbarPage("Navigation",
+             tabPanel("Hist",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput('xcol', 'X Variable', names(iris)),
+                          selectInput('ycol', 'Y Variable', names(iris),
+                                      selected = names(iris)[[2]]),
+                          numericInput('clusters', 'Cluster count', 3, min = 1, max = 3)
+                          ),
+                        mainPanel(plotOutput("plot1"),
+                                  checkboxGroupInput("checkGroup",
+                                                     label = h3("Species"),
+                                                     choices = list(
+                                                       "Setosa" = 1,
+                                                       "Versicolor" = 2,
+                                                       "Virginica" = 3),
+                                                     selected = 1),
+                                  #),
+                   
+                                  headerPanel('Iris k-means clustering')),
+               )),
+  
+  # I commented out the below
+  #  tabPanel("Summary",
+  #    verbatimTextOutput("summary")
+  #  ),
+  #  navbarMenu("Other Information",
+  #    tabPanel("Table",
+  #      DTOutput("iris")
+  #    ),
+    tabPanel("About", fluidRow(column(6, includeMarkdown("about.rmd")),
+                              column(6, img(src='iris.png', #height = , width = , 
+                                            align = "right"),
+                              tags$small("Iris germanica (Purple bearded Iris), Wakehurst Place, UK.  ",
+                              a(href = "http://commons.wikimedia.org/wiki/User:Diliff",
+                              "User: Diliff")))))
+  )
+))
